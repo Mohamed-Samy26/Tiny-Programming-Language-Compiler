01994d852a005e2769373db68e5256a944c65f1a
Mohamed Samy <midoprom@gmail.com>
2022-12-27 00:37:35 +0200

    finalized parser

--- Tiny_Compiler/Parser.cs
+++ Tiny_Compiler/Parser.cs
@@ -568,7 +568,178 @@ namespace Tiny_Compiler
             return writeStatement;
         }
 
-      
+        private Node WriteD()
+        {
+            Node writeD = new Node("Write'");
+            if (InputPointer < TokenStream.Count && TokenStream[InputPointer].token_type == Token_Class.T_Endl)
+            {
+                writeD.Children.Add(match(Token_Class.T_Endl));
+            }
+            else if (InputPointer < TokenStream.Count && isExpression(InputPointer))
+            {
+                writeD.Children.Add(Expression());
+            }
+            else
+                printError("Write Statement");
+            return writeD;
+        }
+
+        private Node AssignmentStatment()
+        {
+            Node assign = new Node("Assignment Statment");
+            assign.Children.Add(match(Token_Class.T_Identifier));
+            assign.Children.Add(match(Token_Class.T_AssignOP));
+            assign.Children.Add(Expression());
+            return assign;
+        }
+
+        private Node DeclarationStatement()
+        {
+            Node declarationStatement = new Node("Declaration Statement");
+            declarationStatement.Children.Add(Datatype());
+            declarationStatement.Children.Add(Identifiers());
+            declarationStatement.Children.Add(match(Token_Class.T_Semicolon));
+            return declarationStatement;
+        }
+
+        private Node Identifiers()
+        {
+            Node ids = new Node("IDs");
+            if (InputPointer + 1 < TokenStream.Count && (TokenStream[InputPointer + 1].token_type == Token_Class.T_Comma || TokenStream[InputPointer + 1].token_type == Token_Class.T_Semicolon))
+            {
+                ids.Children.Add(match(Token_Class.T_Identifier));
+                ids.Children.Add(IdsD());
+            }
+            else if (InputPointer + 1 < TokenStream.Count && TokenStream[InputPointer + 1].token_type == Token_Class.T_AssignOP)
+            {
+                ids.Children.Add(AssignmentStatment());
+                ids.Children.Add(IdsD());
+            }
+            else
+                printError("Identifiers");
+            return ids;
+        }
+
+        private Node IdsD()
+        {
+            Node idsD = new Node("IDs'");
+            if (InputPointer < TokenStream.Count && TokenStream[InputPointer].token_type == Token_Class.T_Comma)
+            {
+                idsD.Children.Add(match(Token_Class.T_Comma));
+                idsD.Children.Add(Identifiers());
+                return idsD;
+            }
+            else
+                return null;
+        }
+
+        private Node ConditionStatement()
+        {
+            Node conditionStatement = new Node("Condition Statement");
+            conditionStatement.Children.Add(Condition());
+            conditionStatement.Children.Add(Conditions());
+            return conditionStatement;
+        }
+
+        private Node Conditions()
+        {
+            Node conditions = new Node("Conditions");
+            if (InputPointer < TokenStream.Count && isBooleanOp(InputPointer))
+            {
+                conditions.Children.Add(BooleanOp());
+                conditions.Children.Add(Condition());
+                conditions.Children.Add(Conditions());
+                return conditions;
+            }
+            else
+                return null;
+        }
+
+        private Node BooleanOp()
+        {
+            Node boolOp = new Node("Boolean Operator");
+            if (InputPointer < TokenStream.Count && TokenStream[InputPointer].token_type == Token_Class.T_AndOP)
+            {
+                boolOp.Children.Add(match(Token_Class.T_AndOP));
+            }
+            else if (InputPointer < TokenStream.Count && TokenStream[InputPointer].token_type == Token_Class.T_OrOP)
+            {
+                boolOp.Children.Add(match(Token_Class.T_OrOP));
+            }
+            else
+                printError("Boolean Operator");
+            return boolOp;
+        }
+
+        private Node Condition()
+        {
+            Node condition = new Node("Condition");
+            condition.Children.Add(match(Token_Class.T_Identifier));
+            condition.Children.Add(ConditionOp());
+            condition.Children.Add(Term());
+
+            return condition;
+        }
+        private Node ConditionOp()
+        {
+            Node condOp = new Node("Condition Operator");
+            if (InputPointer < TokenStream.Count && TokenStream[InputPointer].token_type == Token_Class.T_SmallerThanOP)
+            {
+                condOp.Children.Add(match(Token_Class.T_SmallerThanOP));
+            }
+            else if (InputPointer < TokenStream.Count && TokenStream[InputPointer].token_type == Token_Class.T_GreaterThanOP)
+            {
+                condOp.Children.Add(match(Token_Class.T_GreaterThanOP));
+            }
+            else if (InputPointer < TokenStream.Count && TokenStream[InputPointer].token_type == Token_Class.T_EqualOP)
+            {
+                condOp.Children.Add(match(Token_Class.T_EqualOP));
+            }
+            else if (InputPointer < TokenStream.Count && TokenStream[InputPointer].token_type == Token_Class.T_NotEqualOP)
+            {
+                condOp.Children.Add(match(Token_Class.T_NotEqualOP));
+            }
+            else
+                printError("Condition Operator");
+            return condOp;
+        }
+
+        private Node FunctionCall()
+        {
+            Node funcCall = new Node("Function Call");
+            funcCall.Children.Add(match(Token_Class.T_Identifier));
+            funcCall.Children.Add(match(Token_Class.T_LeftParentheses));
+            funcCall.Children.Add(Arguments());
+            funcCall.Children.Add(match(Token_Class.T_RightParentheses));
+            return funcCall;
+        }
+
+        private Node Arguments()
+        {
+            Node args = new Node("Identifiers");
+            if (InputPointer < TokenStream.Count && isExpression(InputPointer))
+            {
+                args.Children.Add(Expression());
+                args.Children.Add(ArgumentsD());
+                return args;
+            }
+            else
+                return null;
+        }
+
+        private Node ArgumentsD()
+        {
+            Node idD = new Node("Identifier'");
+            if (InputPointer < TokenStream.Count && TokenStream[InputPointer].token_type == Token_Class.T_Comma)
+            {
+                idD.Children.Add(match(Token_Class.T_Comma));
+                idD.Children.Add(Expression());
+                idD.Children.Add(ArgumentsD());
+                return idD;
+            }
+            else
+                return null;
+        }
 
         public static TreeNode PrintParseTree(Node root)
         {
